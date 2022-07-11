b7db0452824d9362180e4ae14e2d6dddf6e0d3d4
Burak <5102072+burki169@users.noreply.github.com>
2022-07-11 21:01:19 +0000

    Intersecting Love Rectangles

---
+++ love-rectangles/Program.cs
@@ -0,0 +1,7 @@
+﻿using Models;
+
+var r1 = new LoveRectangle(1,1,3,4);
+var r2 = new LoveRectangle(2,2,1,1);
+var p = r1.Intersect(r2);
+Console.WriteLine($"{p.BottomLeft} - {p.BottomRight}");
+Console.WriteLine($"{p.TopLeft} - {p.TopRight}");
