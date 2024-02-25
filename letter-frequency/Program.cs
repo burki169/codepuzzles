0c8fca63600c81869c7be88b4912f7ba12844168
Burak <5102072+burki169@users.noreply.github.com>
2024-02-25 07:07:45 +0000

    Solution for Letter Frequency

---
+++ letter-frequency/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+bool EqualizeFrequency(string word){
+    var letterFreq = new Dictionary<char,int>();
+
+    for(int i=0; i<word.Length; i++)
+    {
+        if( letterFreq.ContainsKey(word[i]) )
+            letterFreq[word[i]]++;
+        else
+            letterFreq.Add(word[i], 1);
+    }
+
+    int sum = 0;
+    var values = letterFreq.Values.ToArray();
+    for(int i=0;i<values.Length-2;i++)
+    {
+        sum += values[i] - values[i+1];
+    }
+
+    return sum == 1 || sum == -1;
+    
+}
+
+Console.WriteLine(EqualizeFrequency("abccdd"));
